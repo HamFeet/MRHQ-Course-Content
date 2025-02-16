2393b9069daddb63654a30e6592e34f5e0a87b09
HamFeet <j.a.feetham@gmail.com>
2025-02-16 06:38:40 +0000

    Week 04 code.

--- Week04/Day01-Javascript/app.js
+++ Week04/Day01-Javascript/app.js
@@ -1,1 +1,28 @@
-console.log('Hello from a external script');
+console.log("Hello from a external script");
+// My first variable
+const firstAndLastName = "Joshua Feetham";
+let age = 29;
+
+const ageCalc = 2025 - 1995;
+console.log(firstAndLastName, age, ageCalc);
+
+const question = '"Shall we go?" She said.';
+const manners = 'Remember to say "please" and "thankyou".';
+
+console.log(question, manners);
+
+const quoteOne = '"Concentrate all your thoughts ';
+const quoteTwo = "upon the work in hand. The sun's rays do not burn ";
+const quoteThree = 'until brought the a focus." ';
+const quoteFour = "- Alexander  Graham Bell";
+
+console.log(quoteOne + quoteTwo + quoteThree + quoteFour);
+
+let weight = 61;
+let height = 1.66;
+const BMI = weight / (height * height);
+console.log(BMI);
+
+const bookPrice = 5;
+const totalBooks = 10;
+console.log(bookPrice * totalBooks);
