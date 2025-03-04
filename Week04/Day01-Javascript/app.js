fa2fce85812322d5c0dc75600294d1c777f845f5
HamFeet <j.a.feetham@gmail.com>
2025-03-04 08:04:02 +0000

    Javascript lessons

--- Week04/Day01-Javascript/app.js
+++ Week04/Day01-Javascript/app.js
@@ -1,28 +1,40 @@
-console.log("Hello from a external script");
+// console.log("Hello from a external script");
 // My first variable
-const firstAndLastName = "Joshua Feetham";
-let age = 29;
+// const firstAndLastName = "Joshua Feetham";
+// let age = 29;
 
-const ageCalc = 2025 - 1995;
-console.log(firstAndLastName, age, ageCalc);
+// const birthYear = 1995;
+// let currentYear = 2025;
 
-const question = '"Shall we go?" She said.';
-const manners = 'Remember to say "please" and "thankyou".';
+// const ageCalc = currentYear - birthYear;
+// console.log(ageCalc, birthYear);
 
-console.log(question, manners);
 
-const quoteOne = '"Concentrate all your thoughts ';
-const quoteTwo = "upon the work in hand. The sun's rays do not burn ";
-const quoteThree = 'until brought the a focus." ';
-const quoteFour = "- Alexander  Graham Bell";
+// console.log(firstAndLastName, age, ageCalc);
 
-console.log(quoteOne + quoteTwo + quoteThree + quoteFour);
+// const question = '"Shall we go?" She said.';
+// const manners = 'Remember to say "please" and "thankyou".';
 
-let weight = 61;
-let height = 1.66;
-const BMI = weight / (height * height);
-console.log(BMI);
+// console.log(question, manners);
 
-const bookPrice = 5;
-const totalBooks = 10;
-console.log(bookPrice * totalBooks);
+// const quoteOne = '"Concentrate all your thoughts ';
+// const quoteTwo = "upon the work in hand. The sun's rays do not burn ";
+// const quoteThree = 'until brought the a focus." ';
+// const quoteFour = "- Alexander  Graham Bell";
+
+// console.log(quoteOne + quoteTwo + quoteThree + quoteFour);
+
+// let paragraph = `${quoteOne}${quoteTwo}${quoteThree}${quoteFour}`;
+
+// console.log(paragraph.length);
+
+// let weight = 61;
+// let height = 1.66;
+// const BMI = weight / (height * height);
+// console.log(BMI);
+
+// const bookPrice = 5;
+// const totalBooks = 10;
+// console.log(`She must spend ${bookPrice * totalBooks}`);
+
+
---
+++ Week05/Day01-conditionals/script.js
@@ -0,0 +1,97 @@
+// console.log("Hello world");
+
+// Conditionals If / Else / Else if
+
+let total = 1;
+
+if (total == 1){
+    console.log("Total is positive.");
+}else {
+    console.log("Total is negative.");
+}
+
+const myString = "1 apple";
+
+if (myString[0] === "a"){
+    console.log("True.");
+}else{
+    console.log("False.");
+}
+
+myString[0] === "a" ? console.log(true) : console.log(false);
+
+const word = "hippopotamus";
+
+word.length > 10 ? console.log("the word is long.") : console.log("The word is short.")
+
+let coupon = true;
+let premium = true;
+let userAge = 65;
+
+userAge >= 65 && (premium === true || coupon === true) ? console.log("You are eligible.") : console.log("You are not eligible.");
+
+const dayOfWeek = "Sunday";
+let isWorkDay = false;
+
+switch (dayOfWeek){
+
+    case "Monday":
+        console.log("The day is Monday");
+        isWorkDay = true;
+        break;
+
+    case "Tuesday":
+        console.log("The day is Tuesday");
+        isWorkDay = true;
+        break;
+    case "Wednesday":
+        console.log("The day is Wednesday");
+        isWorkDay = true;
+        break;
+
+    case "Thursday":
+        console.log("The day is Thursday");
+        isWorkDay = true;
+        break;
+
+    case "Friday":
+        console.log("The day is Friday");
+        isWorkDay = true;
+        break;
+
+    case "Saturday":
+        console.log("The day is Saturday");
+        isWorkDay = false;
+        break;
+
+    case "Sunday":
+        console.log("The day is Sunday");
+        isWorkDay = false;
+        break;
+   
+    default:
+        console.log("The day does not exist");
+}
+
+let grade = 100;
+
+switch (grade){
+    case grade >= 90:
+        console.log("You got a A");
+        break;
+    case grade >= 80:
+        console.log("Your grade is a B");
+        break;
+    default:
+        console.log("Your grade is a F");
+        break;
+}
+
+// • Write a variable that hold the numerical grade. Using switch(true) 
+// to console.log the letter grade using the following scale:
+//  • A: 90-100
+//  • B: 80-89
+//  • C: 70-79
+//  • D: 60-69
+//  • F: 0-59
+//  • e.g. If my numerical grade is 55, then I got "F" in the console.
