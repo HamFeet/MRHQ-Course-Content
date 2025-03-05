7e4091da7308bf564e1b506090673ea22dd59272
HamFeet <j.a.feetham@gmail.com>
2025-03-05 06:12:37 +0000

    Finished the code for the lesson

--- Week05/Day02-objects/script.js
+++ Week05/Day02-objects/script.js
@@ -1,1 +1,34 @@
-console.log("Working");
+const cat = {
+    firstName: 'Purr',
+    lastName:'Machine',
+    favouriteColour: "Red",
+    likes: "Cat nip",
+    favouriteFood: "The dogs"
+}
+
+console.log(`This is ${cat.firstName} ${cat.lastName} and their favourite food is ${cat.favouriteFood}.`);
+
+const rectangle = {
+    length: 10,
+    width: 50
+}
+
+console.log(`The area of the rectanlge is ${(rectangle["length"] * rectangle["width"])}`);
+
+const newPerson = {
+    firstName: "Joshua",
+    lastName: "Feetham",
+    favouriteNumber: 23,
+    favouriteDay: "Saturday"
+}
+
+console.log(newPerson);
+
+newPerson.favouriteFood = "Gyros";
+
+console.log(newPerson);
+
+delete newPerson.favouriteDay; 
+delete newPerson.favouriteNumber;
+
+console.log(newPerson);
