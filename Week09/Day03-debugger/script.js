3354cc20cdd9b6577e598e09329e6577237c4d0d
HamFeet <j.a.feetham@gmail.com>
2025-04-03 20:54:58 +1300

    Day two files and completed homework

---
+++ Week09/Day03-debugger/script.js
@@ -0,0 +1,14 @@
+let fruit = "banana";
+
+if (fruit === "banana"){
+    console.log("Fruit is banana");
+}else {
+    console.log("Fruit is not a banana");
+}
+
+
+
+
+// for (let i = 0; i < 6; i++){
+//     console.log(x);
+// }
